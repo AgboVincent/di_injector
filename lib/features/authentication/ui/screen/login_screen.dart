525b9c791a6b6e97839b4d3066a4f0c3a9e97433
teewhydope <ibrahimanimasahun@gmail.com>
2024-11-07 12:58:24 +0100

    initial commit

---
+++ lib/features/authentication/ui/screen/login_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:zojatech_assessment/core/di/injector.dart';
+import 'package:zojatech_assessment/features/authentication/domain/usecase/login_usecase.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final login = locator<LoginUseCase>();
+
+  @override
+  void initState() {
+    super.initState();
+    login.repository.login();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(
+                "Login",
+                style: TextStyle(fontSize: 40.sp),
+              ),
+              SizedBox(height: 20.h),
+              TextField(),
+              SizedBox(height: 20.h),
+              TextField(),
+              SizedBox(height: 60.h),
+              SizedBox(
+                width: 1.sw,
+                child: ElevatedButton(
+                  onPressed: () {
+                    
+                  },
+                  child: Text("Login"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
